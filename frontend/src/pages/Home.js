810ea5a99a0421d0316aaafde54e28acaef91a7a
williamjones <willj836@gmail.com>
2023-01-07 16:47:53 +0000

    added workout form that uses the post route

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
 // components
-
 import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -28,6 +28,7 @@ const Home = () => {
             <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
+      <WorkoutForm />
     </div>
   );
 };
